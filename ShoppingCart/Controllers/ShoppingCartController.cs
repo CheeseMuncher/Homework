7c2693b6f1357904d495b8b3bc9e8126501b7a51
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-16 21:28:31 +0000

    Added ShoppingCartController and Postman Collection

---
+++ ShoppingCart/Controllers/ShoppingCartController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public ShoppingCartController(ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _logger.Information("ShoppingCartController built");
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post([FromBody] ShoppingCartRequest request)
+        {
+            return Ok($"{request.Test} tested");
+        }
+    }
+
+    public class ShoppingCartRequest
+    {
+        public string Test { get; set; }
+    }
+}
