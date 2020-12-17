6e301a473a8304ea3ff09a7e5056dea966c5b2fe
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-17 13:26:58 +0000

    Request validation: negative quantities and coupon code regex

--- ShoppingCart.Tests/ShoppingCartRequestValidatorTests.cs
+++ ShoppingCart.Tests/ShoppingCartRequestValidatorTests.cs
@@ -19,6 +19,17 @@ namespace ShoppingCart.Tests
             result.IsValid.ShouldBeTrue();
         }
 
+        [Test]
+        public void Validate_ReturnsValid_IfZeroQuantitySupplied()
+        {
+            var request = GetValidRequest();
+            request.CartItems = new[] { new CartItem { ProductId = 123, UnitQuantity = 0 } };
+
+            var result = _sut.Validate(request);
+
+            result.IsValid.ShouldBeTrue();
+        }
+
         [TestCase(null)]
         [TestCase("")]
         [TestCase("   ")]
@@ -34,6 +45,37 @@ namespace ShoppingCart.Tests
             result.Errors.Count.ShouldBe(1);
             result.Errors.Single().PropertyName.ShouldBe(nameof(ShoppingCartRequest.CouponCode));
             result.Errors.Single().ErrorMessage.ShouldBe("API takes exactly one Coupon Code");
+        }
+
+        [TestCase("!")]
+        [TestCase("abc#")]
+        [TestCase("123;")]
+        public void Validate_ReturnsInvalid_IfCouponCodeInvalidRegex(string input)
+        {
+            var request = GetValidRequest();
+            request.CouponCode = input;
+
+            var result = _sut.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.Single().PropertyName.ShouldBe(nameof(ShoppingCartRequest.CouponCode));
+            result.Errors.Single().ErrorMessage.ShouldBe("API takes exactly one alphanumeric Coupon Code");
+        }
+
+        [TestCase(-1)]
+        [TestCase(-10)]
+        public void Validate_ReturnsInvalid_IfProductQuantityNoSupplied(int input)
+        {
+            var request = GetValidRequest();
+            request.CartItems = new[] { new CartItem { ProductId = 123, UnitQuantity = input } };
+
+            var result = _sut.Validate(request);
+
+            result.IsValid.ShouldBeFalse();
+            result.Errors.Count.ShouldBe(1);
+            result.Errors.Single().PropertyName.ShouldContain(nameof(CartItem.UnitQuantity));
+            result.Errors.Single().ErrorMessage.ShouldBe("API takes positive quantities");
         }
 
         private ShoppingCartRequest GetValidRequest()
--- ShoppingCart/Models/ValidationMessages.cs
+++ ShoppingCart/Models/ValidationMessages.cs
@@ -3,5 +3,7 @@
     public static class ValidationMessages
     {
         public const string ExactlyOneCode = "API takes exactly one Coupon Code";
+        public const string ExactlyOneCodeAlphanumeric = "API takes exactly one alphanumeric Coupon Code";
+        public const string PositiveQuantities = "API takes positive quantities";
     }
 }
