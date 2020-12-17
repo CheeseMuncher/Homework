906578aa31d5d5d5fcd178972c1666027d07355e
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-17 13:16:36 +0000

    Added DiscountType and Category & Supplier repositories

---
+++ ShoppingCart/Data/SupplierRepository.cs
@@ -0,0 +1,26 @@
+﻿using ShoppingCart.Interfaces;
+using System.Collections.Generic;
+
+namespace ShoppingCart.Data
+{
+    public class SupplierRepository : IRepository
+    {
+        private readonly HashSet<string> _suppliers;
+
+        public SupplierRepository()
+        {
+            // TODO this should come from config or be provided by the ProductRepository
+            _suppliers = new HashSet<string> { "HP", "Dell", "Apple" };
+        }
+
+        public void Add(string item)
+        {
+            _suppliers.Add(item);
+        }
+
+        public bool Contains(string item)
+        {
+            return _suppliers.Contains(item);
+        }
+    }
+}
--- ShoppingCart/Interfaces/IRepository.cs
+++ ShoppingCart/Interfaces/IRepository.cs
@@ -1,9 +1,16 @@
 ﻿namespace ShoppingCart.Interfaces
 {
-
     public interface IRepository<T>
     {
         void Add(T item);
+
         T Get(int id);
+    }
+
+    public interface IRepository
+    {
+        void Add(string item);
+
+        bool Contains(string item);
     }
 }
