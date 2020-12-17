31f06e2f45551eeb1fcd108d2ebf591a2f8925df
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-17 13:26:58 +0000

    Request validation: negative quantities and coupon code regex

---
+++ ShoppingCart/Models/CartItemValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ShoppingCart.Models
+{
+    public class CartItemValidator : AbstractValidator<CartItem>
+    {
+        public CartItemValidator()
+        {
+            RuleFor(item => item.UnitQuantity)
+                .GreaterThan(-1)
+                .WithMessage(ValidationMessages.PositiveQuantities);
+        }
+    }
+}
--- ShoppingCart/Models/ShoppingCartRequestValidator.cs
+++ ShoppingCart/Models/ShoppingCartRequestValidator.cs
@@ -4,6 +4,8 @@ namespace ShoppingCart.Models
 {
     public class ShoppingCartRequestValidator : AbstractValidator<ShoppingCartRequest>
     {
+        private const string _alpahnumericRegex = "^[a-zA-Z0-9_]*$";
+
         public ShoppingCartRequestValidator()
         {
             RuleFor(request => request.CouponCode)
@@ -13,7 +15,12 @@ namespace ShoppingCart.Models
                 .NotEmpty()
                 .WithMessage(ValidationMessages.ExactlyOneCode)
                 .Must(code => code.Split(' ').Length == 1)
-                .WithMessage(ValidationMessages.ExactlyOneCode);
+                .WithMessage(ValidationMessages.ExactlyOneCode)
+                .Matches(_alpahnumericRegex)
+                .WithMessage(ValidationMessages.ExactlyOneCodeAlphanumeric);
+
+            RuleForEach(request => request.CartItems)
+                .SetValidator(new CartItemValidator());
         }
     }
 }
