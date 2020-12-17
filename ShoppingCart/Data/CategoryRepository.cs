103487c290b2cb8cc989e9a8b5e47e9d0bb70875
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-17 13:16:36 +0000

    Added DiscountType and Category & Supplier repositories

---
+++ ShoppingCart/Data/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using ShoppingCart.Interfaces;
+using System.Collections.Generic;
+
+namespace ShoppingCart.Data
+{
+    public class CategoryRepository : IRepository
+    {
+        private readonly HashSet<string> _categories;
+
+        public CategoryRepository()
+        {
+            // TODO this should come from config or be provided by the ProductRepository
+            _categories = new HashSet<string> { "Electronic", "Accessory", "Audio" };
+        }
+
+        public void Add(string item)
+        {
+            _categories.Add(item);
+        }
+
+        public bool Contains(string item)
+        {
+            return _categories.Contains(item);
+        }
+    }
+}
---
+++ ShoppingCart/Models/DiscountType.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Models
+{
+    public enum DiscountType
+    {
+        Supplier = 0,
+        Shipping = 1,
+        Category = 2
+    }
+}
