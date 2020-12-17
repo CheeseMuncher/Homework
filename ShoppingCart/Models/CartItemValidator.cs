385c564e0db41f630d4818a7e5dd1019434df73c
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-17 13:34:32 +0000

    Validating on known products

--- ShoppingCart/Models/CartItemValidator.cs
+++ ShoppingCart/Models/CartItemValidator.cs
@@ -1,14 +1,20 @@
 ﻿using FluentValidation;
+using ShoppingCart.Interfaces;
 
 namespace ShoppingCart.Models
 {
     public class CartItemValidator : AbstractValidator<CartItem>
     {
-        public CartItemValidator()
+        public CartItemValidator(IRepository<Product> productRepository)
         {
             RuleFor(item => item.UnitQuantity)
                 .GreaterThan(-1)
                 .WithMessage(ValidationMessages.PositiveQuantities);
+
+            // Might want to rethink validation strategy if IRepository needs to become async
+            RuleFor(item => item.ProductId)
+                .Must(id => productRepository.Get(id) != null)
+                .WithMessage(ValidationMessages.ProductNotFound);
         }
     }
 }
--- ShoppingCart/Models/ValidationMessages.cs
+++ ShoppingCart/Models/ValidationMessages.cs
@@ -5,5 +5,6 @@
         public const string ExactlyOneCode = "API takes exactly one Coupon Code";
         public const string ExactlyOneCodeAlphanumeric = "API takes exactly one alphanumeric Coupon Code";
         public const string PositiveQuantities = "API takes positive quantities";
+        public const string ProductNotFound = "Product not found";
     }
 }
