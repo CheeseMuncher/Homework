21741288217025205f2350b19fabc569ca28cccc
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-18 12:15:28 +0000

    Added tests for the ShippingCalculator

--- ShoppingCart/Implementation/ShippingCalculator.cs
+++ ShoppingCart/Implementation/ShippingCalculator.cs
@@ -4,10 +4,15 @@ namespace ShoppingCart.Implementation
 {
     public class ShippingCalculator : IShippingCalculator
     {
+        // TODO this should come from config or be provided by a ShippingThresholdRepository
+        private (double value, double price)[] _shippingThresholds = new[] { (20.0, 7.0), (40.0, 5.0) };
+
         public double CalcShipping(double cartTotal)
         {
-            if (cartTotal < 20) return 7;
-            if (cartTotal < 40) return 5;
+            foreach (var threshold in _shippingThresholds)
+                if (cartTotal < threshold.value)
+                    return threshold.price;
+
             return 0;
         }
     }
