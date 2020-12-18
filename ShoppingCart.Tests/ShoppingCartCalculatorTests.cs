dc624b9c962f334a4b67e9aa09b2f5c82cd9d429
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-18 18:11:18 +0000

    Wired up the discount line calculation into ShoppingCartCalculator. Added test cases from readme

--- ShoppingCart.Tests/ShoppingCartCalculatorTests.cs
+++ ShoppingCart.Tests/ShoppingCartCalculatorTests.cs
@@ -20,7 +20,17 @@ namespace ShoppingCart.Tests
             new Product { Id = 5, Name = "iPad", Price = 220, Supplier = "Apple", Categories = new[] { "Electronic" } }
         };
 
+        private Dictionary<string, Discount> _testDiscounts = new Dictionary<string, Discount>
+        {
+            ["BACKTOSCHOOL2019"] = new Discount { DiscountType = DiscountType.Supplier, DiscountedSupplier = "Apple", DiscountPercentage = 15 },
+            ["APPLE5"] = new Discount { DiscountType = DiscountType.Supplier, DiscountedSupplier = "Apple", DiscountPercentage = 5 },
+            ["LOVEMYMUSIC"] = new Discount { DiscountType = DiscountType.Category, DiscountedCategory = "Audio", DiscountPercentage = 20 },
+            ["AUDIO10"] = new Discount { DiscountType = DiscountType.Category, DiscountedCategory = "Audio", DiscountPercentage = 10 },
+            ["FREESHIPPING"] = new Discount { DiscountType = DiscountType.Shipping }
+        };
+
         private Mock<IRepository<int, Product>> _mockProductRepository = new Mock<IRepository<int, Product>>();
+        private Mock<IRepository<string, Discount>> _mockDiscountRepository = new Mock<IRepository<string, Discount>>();
         private ShoppingCartCalculator _sut;
 
         [SetUp]
@@ -30,7 +40,11 @@ namespace ShoppingCart.Tests
                 .Setup(repo => repo.Get(It.IsAny<int>()))
                 .Returns((int id) => _testProducts.FirstOrDefault(p => p.Id == id));
 
-            _sut = new ShoppingCartCalculator(new ShippingCalculator(), _mockProductRepository.Object);
+            _mockDiscountRepository
+                .Setup(repo => repo.Get(It.IsAny<string>()))
+                .Returns((string id) => _testDiscounts.TryGetValue(id, out var value) ? value : null);
+
+            _sut = new ShoppingCartCalculator(new ShippingCalculator(), _mockProductRepository.Object, _mockDiscountRepository.Object);
         }
 
         [Test]
@@ -68,6 +82,45 @@ namespace ShoppingCart.Tests
             var total = _sut.Total(request);
 
             total.ShouldBe(1004);
+        }
+
+        [Test]
+        public void CorrectResult_WithSingleShippingDiscount()
+        {
+            var cart = new List<CartItem> { new CartItem { ProductId = 2, UnitQuantity = 1 } };
+            var request = new ShoppingCartRequest { CouponCode = "FREESHIPPING", CartItems = cart };
+
+            var total = _sut.Total(request);
+
+            total.ShouldBe(4);
+        }
+
+        [TestCase("AUDIO10", 1122)]
+        [TestCase("APPLE5", 1122.8)]
+        [TestCase("FREESHIPPING", 1124)]
+        [TestCase("TRYINGMYLUCK", 1124)]
+        public void CorrectResult_WithTestCaseCart(string couponCode, double expectedTotal)
+        {
+            var cart = GetTestCaseCart(couponCode);
+
+            var result = _sut.Total(cart);
+
+            result.ShouldBe(expectedTotal);
+        }
+
+        private ShoppingCartRequest GetTestCaseCart(string couponCode)
+        {
+            return new ShoppingCartRequest
+            {
+                CouponCode = couponCode,
+                CartItems = new List<CartItem>
+                {
+                    new CartItem { ProductId = 1, UnitQuantity = 2 },
+                    new CartItem { ProductId = 2, UnitQuantity = 1 },
+                    new CartItem { ProductId = 3, UnitQuantity = 1 },
+                    new CartItem { ProductId = 4, UnitQuantity = 1 }
+                }
+            };
         }
     }
 }
--- ShoppingCart/Implementation/ShoppingCartCalculator.cs
+++ ShoppingCart/Implementation/ShoppingCartCalculator.cs
@@ -9,30 +9,29 @@ namespace ShoppingCart.Implementation
     {
         private readonly IShippingCalculator _shippingCalculator;
         private readonly IRepository<int, Product> _productRepository;
+        private readonly IRepository<string, Discount> _discountRepository;
+        private readonly CartItemDiscountCalculator cartItemCalculator = new CartItemDiscountCalculator();
 
         public ShoppingCartCalculator(
             IShippingCalculator shippingCalculator,
-            IRepository<int, Product> productRepository)
+            IRepository<int, Product> productRepository,
+            IRepository<string, Discount> discountRepository)
         {
             _shippingCalculator = shippingCalculator ?? throw new ArgumentException(nameof(shippingCalculator));
             _productRepository = productRepository ?? throw new ArgumentException(nameof(productRepository));
+            _discountRepository = discountRepository ?? throw new ArgumentException(nameof(discountRepository));
         }
 
         public double Total(ShoppingCartRequest request)
         {
-            if (request == null || !request.CartItems.Any()) return 0;
+            if (request.CartItems == null || !request.CartItems.Any()) return 0;
+            var discount = _discountRepository.Get(request.CouponCode ?? "");
 
-            double runningTotal = 0;
-            foreach (var item in request.CartItems)
-            {
-                var product = _productRepository.Get(item.ProductId);
-                if (product != null)
-                {
-                    runningTotal += item.UnitQuantity * product.Price;
-                }
-            }
+            var total = request.CartItems
+                .Select(item => (Product: _productRepository.Get(item.ProductId), item.UnitQuantity))
+                .Sum((item) => cartItemCalculator.CalculateLineTotal(item.Product, item.UnitQuantity, discount));
 
-            return runningTotal + _shippingCalculator.CalcShipping(runningTotal);
+            return discount?.DiscountType == DiscountType.Shipping ? total : total + _shippingCalculator.CalcShipping(total);
         }
     }
 }
