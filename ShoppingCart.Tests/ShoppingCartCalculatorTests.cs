7836583ce0ea9e3990fd6720834f2b3be2a8bcd7
Tom Owen <tomowen84@yahoo.co.uk>
2020-12-18 17:42:46 +0000

    Refactored existing ShoppingCartCalculator tests

--- ShoppingCart.Tests/ShoppingCartCalculatorTests.cs
+++ ShoppingCart.Tests/ShoppingCartCalculatorTests.cs
@@ -1,46 +1,70 @@
+using Moq;
 using NUnit.Framework;
-using ShoppingCart.Data;
 using ShoppingCart.Implementation;
+using ShoppingCart.Interfaces;
 using ShoppingCart.Models;
+using Shouldly;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingCart.Tests
 {
     public class ShoppingCartCalculatorTests
     {
-        private ProductRepository _productRepository;
+        private Product[] _testProducts = new[]
+        {
+            new Product { Id = 1, Name = "Headphones", Price = 10, Supplier = "Apple", Categories = new[] { "Accessory", "Electronic", "Audio" } },
+            new Product { Id = 2, Name = "USB Cable", Price = 4, Supplier = "Apple", Categories = new[] { "Accessory" } },
+            new Product { Id = 3, Name = "Monitor", Price = 100, Supplier = "HP", Categories = new[] { "Electronic" } },
+            new Product { Id = 4, Name = "Laptop", Price = 1000, Supplier = "Dell", Categories = new[] { "Electronic" } },
+            new Product { Id = 5, Name = "iPad", Price = 220, Supplier = "Apple", Categories = new[] { "Electronic" } }
+        };
+
+        private Mock<IRepository<int, Product>> _mockProductRepository = new Mock<IRepository<int, Product>>();
+        private ShoppingCartCalculator _sut;
 
         [SetUp]
         public void Init()
         {
-            _productRepository = new ProductRepository();
+            _mockProductRepository
+                .Setup(repo => repo.Get(It.IsAny<int>()))
+                .Returns((int id) => _testProducts.FirstOrDefault(p => p.Id == id));
+
+            _sut = new ShoppingCartCalculator(new ShippingCalculator(), _mockProductRepository.Object);
         }
 
         [Test]
-        public void WithSmallCart_CheckCalculation()
+        public void CorrectResult_WithSingleItemAndShipping()
         {
-            var carItem1 = new CartItem { ProductId = 2, UnitQuantity = 1 };
-            var carItem2 = new CartItem { ProductId = 4, UnitQuantity = 1 };
+            var cart = new List<CartItem> { new CartItem { ProductId = 2, UnitQuantity = 1 } };
 
-            var cart = new List<CartItem> { carItem1, carItem2 };
+            var total = _sut.Total(cart);
 
-            var calc = new ShoppingCartCalculator(new ShippingCalculator(), _productRepository);
-            double total = calc.Total(cart);
-
-            Assert.AreEqual(1004, total);
+            total.ShouldBe(11);
         }
 
         [Test]
-        public void WithOneItem_CheckCalculation()
+        public void CorrectResult_WithMultipleQuantity()
         {
-            var carItem1 = new CartItem { ProductId = 2, UnitQuantity = 1 };
+            var cart = new List<CartItem> { new CartItem { ProductId = 3, UnitQuantity = 2 } };
 
-            var cart = new List<CartItem> { carItem1 };
+            var total = _sut.Total(cart);
 
-            var calc = new ShoppingCartCalculator(new ShippingCalculator(), _productRepository);
-            double total = calc.Total(cart);
+            total.ShouldBe(200);
+        }
 
-            Assert.AreEqual(11, total);
+        [Test]
+        public void CorrectResult_WithMultipleCartItems()
+        {
+            var cart = new List<CartItem>
+            {
+                new CartItem { ProductId = 2, UnitQuantity = 1 },
+                new CartItem { ProductId = 4, UnitQuantity = 1 }
+            };
+
+            var total = _sut.Total(cart);
+
+            total.ShouldBe(1004);
         }
     }
 }
