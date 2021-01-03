ad41e80aea76b13601e2ba32417c590a7f4fa9b2
Tom Owen <tomowen84@yahoo.co.uk>
2021-01-03 13:43:24 +0000

    Added a lightweight validation object

---
+++ MarsRover/MarsRover/ValidationResult.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MarsRover
+{
+    public class ValidationResult
+    {
+        public bool IsValid => !Errors.Any();
+
+        public IEnumerable<string> Errors { get; private set; } = new List<string>();
+
+        public ValidationResult(string[] errors)
+        {
+            Errors = errors ?? new string[0];
+        }
+
+        public ValidationResult()
+        {
+            Errors = new string[0];
+        }
+    }
+
+    public static class ValidationResultExtensions
+    {
+        public static ValidationResult Combine(this ValidationResult result, ValidationResult otherResult)
+        {
+            return otherResult == null
+                ? result
+                : new ValidationResult(result.Errors.Concat(otherResult.Errors).ToArray());
+        }
+    }
+}
