99697947cc0359f3211cb6270667ec2468863d73
Tom Owen <tomowen84@yahoo.co.uk>
2021-01-03 13:30:49 +0000

    Program using hard coded input to generate the expected output

--- MarsRover/MarsRover/Program.cs
+++ MarsRover/MarsRover/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MarsRover
 {
@@ -7,6 +8,33 @@ namespace MarsRover
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello Mars!");
+
+            var input = new[] { "5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM" };
+            var output = new List<string>();
+            var index = 0;
+            RoverPosition currentRover = null;
+            Console.WriteLine("Input:");
+            foreach (var line in input)
+            {
+                Console.WriteLine(line);
+                if (index == 0)
+                {
+                    index++;
+                    continue;
+                }
+
+                if (index % 2 == 1)
+                    currentRover = new RoverPosition(line);
+
+                if (index % 2 == 0)
+                    output.Add(currentRover.ExecuteRoute(line));
+
+                index++;
+            }
+
+            Console.WriteLine("Output:");
+            foreach (var line in output)
+                Console.WriteLine(line);
         }
     }
 }
