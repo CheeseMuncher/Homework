3459842639b19ebb023d240e832c49baebeb20f6
Tom Owen <tomowen84@yahoo.co.uk>
2021-01-03 13:43:24 +0000

    Added a lightweight validation object

---
+++ MarsRover/MarsRoverTests/ValidationResultTests.cs
@@ -0,0 +1,71 @@
+﻿using MarsRover;
+using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace MarsRoverTests
+{
+    public class ValidationResultTests
+    {
+        [Fact]
+        public void ValidationResult_HandlesNullConstructorInput()
+        {
+            var sut = new ValidationResult(null);
+
+            var _ = sut.IsValid;
+
+            // test passes by virtue of no NullReferenceException being thrown
+        }
+
+        [Fact]
+        public void ValidationResult_IsValid_IfNoErrors()
+        {
+            var sut = new ValidationResult();
+
+            var result = sut.IsValid;
+
+            result.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ValidationResult_IsValid_IfItContainsErrors()
+        {
+            var sut = new ValidationResult(new[] { "Testing" });
+
+            var result = sut.IsValid;
+
+            result.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ValidationResult_ExposesAllErrors()
+        {
+            var sut = new ValidationResult(new[] { "Testing1", "Testing2" });
+
+            var result = sut.Errors;
+
+            result.Count().ShouldBe(2);
+        }
+
+        [Fact]
+        public void Combine_ShouldMergeErrorCollections()
+        {
+            var result1 = new ValidationResult(new[] { "Testing1", "Testing2" });
+            var result2 = new ValidationResult(new[] { "Testing3" });
+
+            var result = result1.Combine(result2);
+
+            result.Errors.Count().ShouldBe(3);
+        }
+
+        [Fact]
+        public void Combine_ShouldHandleNullInput()
+        {
+            var result1 = new ValidationResult(new[] { "Testing1", "Testing2" });
+
+            var result = result1.Combine(null);
+
+            result.Errors.Count().ShouldBe(2);
+        }
+    }
+}
