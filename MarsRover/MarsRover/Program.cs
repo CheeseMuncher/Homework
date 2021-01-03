4cea96cfc2dbef5d11de82f3e70de1d604d449b2
Tom Owen <tomowen84@yahoo.co.uk>
2021-01-03 12:49:57 +0000

    Added empty console app with readme as solution item

---
+++ MarsRover/MarsRover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsRover
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mars!");
+        }
+    }
+}
