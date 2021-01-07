1de93dfe669f8acd5921acef2bf77237ede4db28
Tom Owen <tomowen84@yahoo.co.uk>
2021-01-07 14:39:08 +0000

    Dry violation

--- ShoppingCart/ShoppingCart/Implementation/CartItemDiscountCalculator.cs
+++ ShoppingCart/ShoppingCart/Implementation/CartItemDiscountCalculator.cs
@@ -12,19 +12,26 @@ namespace ShoppingCart.Implementation
             switch (discount?.DiscountType)
             {
                 case DiscountType.Supplier:
-                    return item.Supplier.Equals(discount.DiscountedSupplier, StringComparison.InvariantCultureIgnoreCase)
-                        ? quantity * item.Price * (1 - discount.DiscountPercentage / 100)
-                        : quantity * item.Price;
+                    return CalculateLineTotal(quantity, item.Price, discount.DiscountPercentage, ApplySupplierDiscount(item.Supplier, discount.DiscountedSupplier));
 
                 case DiscountType.Category:
-                    return item.Categories.Any(cat => cat.Equals(discount.DiscountedCategory, StringComparison.InvariantCultureIgnoreCase))
-                        ? quantity * item.Price * (1 - discount.DiscountPercentage / 100)
-                        : quantity * item.Price;
+                    return CalculateLineTotal(quantity, item.Price, discount.DiscountPercentage, ApplyCategoryDiscount(item.Categories, discount.DiscountedCategory));
 
                 case DiscountType.Shipping:
                 default:
-                    return quantity * item.Price;
+                    return CalculateWithoutDiscount(quantity, item.Price);
             }
         }
+
+        private bool ApplySupplierDiscount(string itemSupplier, string discountSupplier) => itemSupplier.Equals(discountSupplier, StringComparison.InvariantCultureIgnoreCase);
+
+        private bool ApplyCategoryDiscount(string[] itemCategories, string discountCategory) => itemCategories.Any(cat => cat.Equals(discountCategory, StringComparison.InvariantCultureIgnoreCase));
+
+        private decimal CalculateWithoutDiscount(int quantity, decimal price) => quantity * price;
+
+        private decimal CalculateWithDiscount(int quantity, decimal price, decimal percentage) => quantity * price * (1 - percentage / 100);
+
+        private decimal CalculateLineTotal(int quantity, decimal price, decimal percentage, bool applyDiscount)
+            => applyDiscount ? CalculateWithDiscount(quantity, price, percentage) : CalculateWithoutDiscount(quantity, price);
     }
 }
