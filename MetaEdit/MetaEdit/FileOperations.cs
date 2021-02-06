d6665ee258125483f2f88e3c8d1badb306e27c2f
Tom Owen <tomowen84@yahoo.co.uk>
2021-02-06 22:11:37 +0000

    Added File operations

---
+++ MetaEdit/MetaEdit/FileOperations.cs
@@ -0,0 +1,41 @@
+﻿using MetaEdit.Conventions;
+using System.IO;
+using System.Linq;
+
+namespace MetaEdit
+{
+    public class FileOperations : IFileOperations
+    {
+        private readonly IDecodeConvention _convention;
+
+        public FileOperations(IDecodeConvention convention)
+        {
+            _convention = convention;
+        }
+
+        public string[] GetFiles(string relativePath)
+        {
+            return _convention.Extensions.SelectMany(ext => Directory
+                .GetFiles(relativePath)
+                .Where(file => file.EndsWith(ext)))
+                .ToArray();
+        }
+
+        public bool TryExtractFileName(string path, out string name)
+        {
+            name = null;
+            if (string.IsNullOrEmpty(path) || !PathInExpectedFormat(path))
+                return false;
+
+            name = path.Split(Path.DirectorySeparatorChar.ToString()).Last().Split(".").First();
+            return true;
+        }
+
+        private bool PathInExpectedFormat(string path)
+        {
+            var file = Path.GetFileName(path);
+            return !string.IsNullOrEmpty(file)
+                && _convention.Extensions.Any(ext => file.EndsWith(ext));
+        }
+    }
+}
---
+++ MetaEdit/MetaEdit/IFileOperations.cs
@@ -0,0 +1,9 @@
+﻿namespace MetaEdit
+{
+    public interface IFileOperations
+    {
+        string[] GetFiles(string relativePath);
+
+        bool TryExtractFileName(string path, out string name);
+    }
+}
