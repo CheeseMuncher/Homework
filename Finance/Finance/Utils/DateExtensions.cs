a0cd6e040d65d03091c704b84c16138c29167c3f
CheeseMuncher <tomowen84@yahoo.co.uk>
2021-12-25 22:16:33 +0000

    Renamed holiday methods, changed xmas logic

--- Finance/Finance/Utils/DateExtensions.cs
+++ Finance/Finance/Utils/DateExtensions.cs
@@ -6,8 +6,10 @@ namespace Finance.Utils;
 public static class DateExtensions
 {
     public static bool IsWeekday(this DateTime day) => day.DayOfWeek != DayOfWeek.Saturday && day.DayOfWeek != DayOfWeek.Sunday;
-    public static bool IsXmas(this DateTime day) => day.Month == 12 && day.Day == 25;
-    public static bool IsFirstWorkday(this DateTime day) => day.Month == 1
+    public static bool IsXmasHoliday(this DateTime day) => day.Month == 12
+        && ((day.Day == 25 && day.IsWeekday())
+            || (day.DayOfWeek == DayOfWeek.Monday && (day.Day == 26 || day.Day == 27)));
+    public static bool IsNewYearHoliday(this DateTime day) => day.Month == 1
         && ((day.Day == 1 && day.IsWeekday())
-             || (day.Day <= 3 && day.DayOfWeek == DayOfWeek.Monday));
+            || (day.Day <= 3 && day.DayOfWeek == DayOfWeek.Monday));
 }
