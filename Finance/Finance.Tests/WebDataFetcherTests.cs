750421acda28548765f6f0a462f2646045476e00
CheeseMuncher <tomowen84@yahoo.co.uk>
2021-12-27 22:39:38 +0000

    Fixed tests

--- Finance/Finance.Tests/WebDataFetcherTests.cs
+++ Finance/Finance.Tests/WebDataFetcherTests.cs
@@ -15,9 +15,9 @@ namespace Finance.Tests;
 
 public class WebDataFetcherTests : TestFixture<WebDataFetcher>
 {
-    private Mock<IFileIO> _mockFileIO = new Mock<IFileIO>();
-    private Mock<IHttpRequestFactory> _mockFactory = new Mock<IHttpRequestFactory>();
-    private Mock<HttpMessageHandler> _mockHandler = new Mock<HttpMessageHandler>();
+    private readonly Mock<IFileIO> _mockFileIO = new Mock<IFileIO>();
+    private readonly Mock<IHttpRequestFactory> _mockFactory = new Mock<IHttpRequestFactory>();
+    private readonly Mock<HttpMessageHandler> _mockHandler = new Mock<HttpMessageHandler>();
 
     public WebDataFetcherTests()
     {
@@ -27,7 +27,7 @@ public class WebDataFetcherTests : TestFixture<WebDataFetcher>
 
         _mockHandler.Protected()            
             .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-            .ReturnsAsync(new HttpResponseMessage { });
+            .ReturnsAsync(new HttpResponseMessage { Content = new StringContent("{}")});
 
         Inject(_mockFileIO.Object);
         Inject(_mockFactory.Object);
@@ -53,10 +53,10 @@ public class WebDataFetcherTests : TestFixture<WebDataFetcher>
                 startPayload = d1;
                 endPayload = d2;
             })
-            .Returns(Create<HttpRequestMessage>());        
+            .Returns(Create<HttpRequestMessage>());
 
         // Act
-        await Sut.GetYahooApiData(stock, start, end);        
+        await Sut.GetYahooApiData(stock, start, end);
 
         // Assert
         stockPayload.Should().Be(stock);
@@ -67,79 +67,80 @@ public class WebDataFetcherTests : TestFixture<WebDataFetcher>
     [Fact]
     public async void GetYahooApiData_SendsFactoryRequestToApi()
     {
-            // Arrange
-            var factoryRequest = Create<HttpRequestMessage>();
-            _mockFactory
-                .Setup(factory => factory.GetYahooFinanceRequest(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>()))
-                .Returns(factoryRequest);        
+        // Arrange
+        var factoryRequest = Create<HttpRequestMessage>();
+        _mockFactory
+            .Setup(factory => factory.GetYahooFinanceRequest(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>()))
+            .Returns(factoryRequest);        
 
-            HttpRequestMessage payload = null!;
-            _mockHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .Callback((HttpRequestMessage hrm, CancellationToken ct) => payload = hrm)
-                .ReturnsAsync(new HttpResponseMessage { });
+        HttpRequestMessage payload = null!;
+        _mockHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .Callback((HttpRequestMessage hrm, CancellationToken ct) => payload = hrm)
+            .ReturnsAsync(new HttpResponseMessage { Content = new StringContent("{}")});
 
-            // Act
-            await Sut.GetYahooApiData(Create<string>(), Create<double>(), Create<double>());
+        // Act
+        await Sut.GetYahooApiData(Create<string>(), Create<double>(), Create<double>());
 
-            // Assert
-            payload.Should().NotBeNull();
-            payload.Should().BeEquivalentTo(factoryRequest);
+        // Assert
+        payload.Should().NotBeNull();
+        payload.Should().BeEquivalentTo(factoryRequest);
     }
 
     [Fact]
     public async void GetYahooApiData_DoesNotWriteResponseDataToFile_IfWriteFlagNotSet()
     {
-            // Arrange
+        // Arrange
 
-            // Act
-            await Sut.GetYahooApiData(Create<string>(), Create<double>(), Create<double>());
+        // Act
+        await Sut.GetYahooApiData(Create<string>(), Create<double>(), Create<double>());
 
-            // Assert
-            _mockFileIO.Verify(io => io.WriteText(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
-            _mockFileIO.VerifyNoOtherCalls();
+        // Assert
+        _mockFileIO.Verify(io => io.WriteText(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        _mockFileIO.VerifyNoOtherCalls();
     }
 
     [Fact]
     public async void GetYahooApiData_WritesResponseDataToFile_IfFlagSet()
     {
-            // Arrange
-            var stock = Create<string>();
-            var content = Create<string>();
-            _mockHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())                
-                .ReturnsAsync(new HttpResponseMessage 
-                { 
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent(content)
-                });
+        // Arrange
+        var stock = Create<string>();
+        var response = Create<Response>();
+        var content = JsonSerializer.Serialize(response);
+        _mockHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())                
+            .ReturnsAsync(new HttpResponseMessage 
+            { 
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(content)
+            });
 
-            // Act
-            await Sut.GetYahooApiData(stock, Create<double>(), Create<double>(), true);
+        // Act
+        await Sut.GetYahooApiData(stock, Create<double>(), Create<double>(), true);
 
-            // Assert
-            _mockFileIO.Verify(io => io.WriteText(content, It.Is<string>(str => str.Contains(stock))), Times.Once);
-            _mockFileIO.VerifyNoOtherCalls();
+        // Assert
+        _mockFileIO.Verify(io => io.WriteText(content, It.Is<string>(str => str.Contains(stock))), Times.Once);
+        _mockFileIO.VerifyNoOtherCalls();
     }
 
     [Fact]
     public async void GetYahooApiData_DeserialisesResponseData()
     {
-            // Arrange
-            var response = Create<Response>();
-            var content = JsonSerializer.Serialize(response);
-            _mockHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())                
-                .ReturnsAsync(new HttpResponseMessage 
-                { 
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent(content)
-                });
+        // Arrange
+        var response = Create<Response>();
+        var content = JsonSerializer.Serialize(response);
+        _mockHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())                
+            .ReturnsAsync(new HttpResponseMessage 
+            { 
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(content)
+            });
 
-            // Act
-            var result = await Sut.GetYahooApiData(Create<string>(), Create<double>(), Create<double>(), true);
+        // Act
+        var result = await Sut.GetYahooApiData(Create<string>(), Create<double>(), Create<double>(), true);
 
-            // Assert
-            result.Should().BeEquivalentTo(response);
+        // Assert
+        result.Should().BeEquivalentTo(response);
     }
 }
