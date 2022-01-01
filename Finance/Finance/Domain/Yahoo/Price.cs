d3f700077ac9d9d05da23d3a795397ec873acdfa
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-01-01 21:38:19 +0000

    Handling nulls in the yahoo data

--- Finance/Finance/Domain/Yahoo/Price.cs
+++ Finance/Finance/Domain/Yahoo/Price.cs
@@ -3,10 +3,10 @@ namespace Finance.Domain.Yahoo;
 public class Price
 {
     public long date { get; set; }
-    public decimal open { get; set; }
-    public decimal high { get; set; }
-    public decimal low { get; set; }
-    public decimal close { get; set; }
-    public decimal volume { get; set; }
-    public decimal adjclose { get; set; }
+    public decimal? open { get; set; } = 0m;
+    public decimal? high { get; set; }
+    public decimal? low { get; set; }
+    public decimal close { get; set; } = 0m;
+    public decimal? volume { get; set; }
+    public decimal? adjclose { get; set; }
 }
