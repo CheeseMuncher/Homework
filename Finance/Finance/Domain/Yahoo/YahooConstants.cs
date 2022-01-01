1f9b7dbc665a18749b05a7ff633b784bfa82b729
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-01-01 22:35:30 +0000

    Renamed some constants

--- Finance/Finance/Domain/Yahoo/YahooConstants.cs
+++ Finance/Finance/Domain/Yahoo/YahooConstants.cs
@@ -3,10 +3,9 @@ namespace Finance.Domain.Yahoo;
 public class YahooConstants
 {
     public static readonly string BasePath = "https://yh-finance.p.rapidapi.com/";
-   
-    public static readonly string HeaderKeyKey = "x-rapidapi-key";
-    public static readonly string HeaderHostKey = "x-rapidapi-host";
-    public static readonly string HeaderHostValue = "yh-finance.p.rapidapi.com";    
+    public static readonly string RapidApiHeaderKey = "x-rapidapi-key";
+    public static readonly string HostHeaderKey = "x-rapidapi-host";
+    public static readonly string HostHeaderValue = "yh-finance.p.rapidapi.com";    
 
     public static class Endpoints
     {
