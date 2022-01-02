864f054d620781b2ec9a95bcbfd018a378253cb1
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-01-02 14:21:37 +0000

    DataManager fetching chart data

--- Finance/Finance/Domain/Yahoo/YahooDataManager.cs
+++ Finance/Finance/Domain/Yahoo/YahooDataManager.cs
@@ -10,29 +10,36 @@ public class YahooDataManager
     private readonly IFileDataClient _fileClient;
     private readonly IFileIO _fileIO;
 
+    private Func<PriceSet, string, DateTime[], long, long, bool, Task> _historyDataDelegate;
+    private Func<PriceSet, string, DateTime[], long, long, bool, Task> _chartDataDelegate;
+
     public YahooDataManager(IWebDataClient webClient, IFileDataClient fileClient, IFileIO fileIO)
     {
         _webClient = webClient ?? throw new NullReferenceException(nameof(webClient));
         _fileClient = fileClient ?? throw new NullReferenceException(nameof(fileClient));
         _fileIO = fileIO ?? throw new NullReferenceException(nameof(fileIO));
+
+        _historyDataDelegate = async (PriceSet prices, string stock, DateTime[] dates, long start, long end, bool writeRawData) =>
+        {
+            var response = await _webClient.GetYahooHistoryData(stock, start, end, writeRawData);            
+            prices = prices.AddPrices(response.ToPriceSet(dates, stock.HandleSuffix()), stock.HandleSuffix());
+        };
+
+        _chartDataDelegate = async (PriceSet prices, string stock, DateTime[] dates, long start, long end, bool writeRawData) =>
+        {
+            var response = await _webClient.GetYahooChartData(stock, start, end, writeRawData);
+            prices = prices.AddPrices(response.chart.result.Single().ToPriceSet(dates), stock.HandleSuffix());
+        };
     }
 
     public async Task GeneratePriceHistoryDataFromApi(DateTime[] dates, string[] stocks, bool writeRawData = false)
     {
-        var start = (long)dates.OrderBy(d => d).First().ToUnixTimeStamp();
-        var end = (long)dates.OrderBy(d => d).Last().ToUnixTimeStamp();
-        var allPrices = new PriceSet();
-        foreach(var stock in stocks)
-        {
-            var response = await _webClient.GetYahooHistoryData(stock, start, end, writeRawData);            
-            allPrices = allPrices.AddPrices(response.ToPriceSet(dates, stock.HandleSuffix()), stock.HandleSuffix());
-        }
-        var trimmedStocks = stocks.Select(s => s.HandleSuffix()).ToArray();
-        
-        _fileIO.WriteText(allPrices
-            .Interpolate(trimmedStocks)
-            .ConvertToCsv(QuoteKeys.Headers), 
-                FileNameTemplate($"Prices_{string.Join("_", trimmedStocks)}", ".csv"));
+        await GeneratePriceDataFromApi(_historyDataDelegate, dates, stocks, writeRawData);
+    }
+
+    public async Task GeneratePriceChartDataFromApi(DateTime[] dates, string[] stocks, bool writeRawData = false)
+    {
+        await GeneratePriceDataFromApi(_chartDataDelegate, dates, stocks, writeRawData);
     }
 
     public void GeneratePriceHistoryDataFromFile(string fileName, string stock)
@@ -44,6 +51,22 @@ public class YahooDataManager
                 FileNameTemplate($"Prices_{stock}", ".csv"));
     }
 
+    private async Task GeneratePriceDataFromApi(Func<PriceSet, string, DateTime[], long, long, bool, Task> dataDelegate, DateTime[] dates, string[] stocks, bool writeRawData = false)
+    {
+        var start = (long)dates.OrderBy(d => d).First().ToUnixTimeStamp();
+        var end = (long)dates.OrderBy(d => d).Last().ToUnixTimeStamp();
+        var allPrices = new PriceSet();
+        foreach(var stock in stocks)
+            await dataDelegate.Invoke(allPrices, stock, dates, start, end, writeRawData);
+
+        var trimmedStocks = stocks.Select(s => s.HandleSuffix()).ToArray();
+        
+        _fileIO.WriteText(allPrices
+            .Interpolate(stocks)
+            .ConvertToCsv(QuoteKeys.Headers), 
+                FileNameTemplate($"Prices_{string.Join("_", trimmedStocks)}", ".csv"));
+    }
+
     private static string FileNameTemplate(string datatype, string extension) =>
-            $"{DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss")}_{datatype}{extension}";
+        $"{DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss")}_{datatype}{extension}";
 }
