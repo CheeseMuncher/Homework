bad580a23716a0f383571f7debbfd354ef9879db
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-01-02 10:53:21 +0000

    Re-added the Chart endpoint response models

---
+++ Finance/Finance/Domain/Yahoo/Models/Result.cs
@@ -0,0 +1,9 @@
+namespace Finance.Domain.Yahoo.Models;
+
+public class Result
+{
+    public Dictionary<string, object> meta {get;set;} = null!;
+    public long[] timestamp {get;set;} = null!;
+    public Indicators indicators {get;set;} = null!;
+    public DividendSet events {get;set;} = null!;
+}
