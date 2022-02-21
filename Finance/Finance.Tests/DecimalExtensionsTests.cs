aac6f1c88102c9acdc508cadf45c10b2a504c837
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-02-21 22:37:18 +0000

    Allowing nulls on the close

--- Finance/Finance.Tests/DecimalExtensionsTests.cs
+++ Finance/Finance.Tests/DecimalExtensionsTests.cs
@@ -7,16 +7,30 @@ namespace Finance.Tests;
 public class DecimalExtensionsExtensionsTests : TestFixture
 {
     [Theory]
+    [InlineData(0.0, 6, 0.0)]
     [InlineData(0.123456789, 6, 0.123457)]
     [InlineData(0.123456111, 6, 0.123456)]
     [InlineData(0.123456789, 7, 0.1234568)]
-    public void IsWeekday_ReturnsFalse_IfWeekend(decimal input, int digits, decimal output)
+    public void RoundToSignificantDigits_PerformsRoundingCorrectly(decimal input, int digits, decimal output)
     {
         // Arrange
         // Act
-        var result = input.RoundToSignificantDigits(digits);
+        var result = (input as decimal?).RoundToSignificantDigits(digits);
 
         // Assert
         result.Should().Be(output);
+    }
+
+    [Fact]
+    public void RoundToSignificantDigits_HandlesNull()
+    {
+        // Arrange
+        decimal? input = null;
+
+        // Act
+        var result = input.RoundToSignificantDigits(2);
+
+        // Assert
+        result.Should().Be(0);
     }
 }
