85060277107922a065f0ae182b7d832d3c02b7b8
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-07-06 00:16:28 +0100

    Paymentsense Coding Challenge Submission 2021-01-19 (#6)

--- CodingChallenge/paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Startup.cs
+++ CodingChallenge/paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Startup.cs
@@ -2,7 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Paymentsense.Coding.Challenge.Api.Services;
+using System;
+using System.Net.Http;
 
 namespace Paymentsense.Coding.Challenge.Api
 {
@@ -18,6 +23,15 @@ namespace Paymentsense.Coding.Challenge.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var apiConfig = new ApiConfig();
+            Configuration.GetSection("ApiConfig").Bind(apiConfig);
+            services.AddSingleton(apiConfig);
+
+            services.TryAddTransient(s => s.GetRequiredService<IHttpClientFactory>().CreateClient(string.Empty));
+            services.AddHttpClient<IRestCountriesApi, RestCountriesApi>().SetHandlerLifetime(TimeSpan.FromMinutes(5));
+            services.AddSingleton<IRestCountriesApi, RestCountriesApi>();
+            services.AddSingleton<ICountryService, CachedCountryService>();
+            services.AddMemoryCache();
             services.AddControllers();
             services.AddHealthChecks();
             services.AddCors(options =>
@@ -28,6 +42,11 @@ namespace Paymentsense.Coding.Challenge.Api
                         .AllowAnyMethod()
                         .AllowAnyHeader();
                 });
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = $"{Program.ApplicationName}", Version = $"{Program.ApplicationVersion}" });
+                c.EnableAnnotations();
             });
         }
 
@@ -52,6 +71,8 @@ namespace Paymentsense.Coding.Challenge.Api
                 endpoints.MapControllers();
                 endpoints.MapHealthChecks("/health");
             });
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{Program.ApplicationName} v{Program.ApplicationVersion}"));
         }
     }
 }
--- CodingChallenge/paymentsense-coding-challenge-website/src/app/services/paymentsense-coding-challenge-api.service.ts
+++ CodingChallenge/paymentsense-coding-challenge-website/src/app/services/paymentsense-coding-challenge-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { ICountry } from '../models/country';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ export class PaymentsenseCodingChallengeApiService {
   constructor(private httpClient: HttpClient) {}
 
   public getHealth(): Observable<string> {
-    return this.httpClient.get('https://localhost:44341/health', { responseType: 'text' });
+    return this.httpClient.get('https://localhost:5001/health', { responseType: 'text' });
+  }
+
+  public getCountries(): Observable<ICountry[]> {
+    return this.httpClient.get<ICountry[]>('https://localhost:5001/countries');
   }
 }
