569d84dacadbb3e74c5c4807324c02cc53846a32
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-08-15 15:21:53 +0100

    Added empty PdfUtil solution

---
+++ PdfUtil/PdfUtil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PdfUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
