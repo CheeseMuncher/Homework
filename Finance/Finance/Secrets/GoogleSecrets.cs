8bc798afedf74997aef33bc59a8149e484ee463d
CheeseMuncher <31480445+CheeseMuncher@users.noreply.github.com>
2022-12-22 00:47:20 +0000

    Adding Secrets to Finance (#16)

---
+++ Finance/Finance/Secrets/GoogleSecrets.cs
@@ -0,0 +1,6 @@
+namespace Finance;
+
+public static class GoogleSecrets
+{
+    public const string KeyFileName = "ServiceAccountKey.json";
+}
---
+++ Finance/Finance/Secrets/YahooSecrets.cs
@@ -0,0 +1,6 @@
+namespace Finance.Secrets;
+
+public static class YahooSecrets
+{
+    public const string RapidApiHeaderValue = "CopyPasteMe";
+}
