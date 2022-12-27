649d812f1b708de46784436b2d7b5b09d9c8cb33
CheeseMuncher <31480445+CheeseMuncher@users.noreply.github.com>
2022-12-27 21:39:28 +0000

    Adding FileIO.SecretsFileExists (#17)

--- Finance/Finance/Utils/FileIO.cs
+++ Finance/Finance/Utils/FileIO.cs
@@ -1,14 +1,20 @@
-using Finance.Domain.Prices;
-
 namespace Finance.Utils;
+
+public interface IFileIO
+{
+    bool DataFileExists(string fileName);
+    bool SecretsFileExists(string fileName);
+    IEnumerable<string> ReadLinesFromFile(string fileName);
+    void WriteText(string text, string fileName);
+}
 
 public class FileIO : IFileIO
 {
-    public bool FileExists(string fileName) => File.Exists(GetDataPath(fileName));
-
+    public bool DataFileExists(string fileName) => File.Exists(GetDataPath(fileName));
+    public bool SecretsFileExists(string fileName) => File.Exists(GetSecretsPath(fileName));
     public IEnumerable<string> ReadLinesFromFile(string fileName)
     {
-        if (!FileExists(fileName))
+        if (!DataFileExists(fileName))
             yield break;                
 
         string? s;
@@ -16,7 +22,6 @@ public class FileIO : IFileIO
             while ((s = sr.ReadLine()) != null)
                 yield return s;
     }
-
     public void WriteText(string text, string fileName)
     {
         using (StreamWriter sw = File.CreateText(GetDataPath(fileName)))
@@ -24,11 +29,5 @@ public class FileIO : IFileIO
     }
 
     private static string GetDataPath(string fileName) => Path.Combine("FinanceData", fileName);
-}
-
-public interface IFileIO
-{
-    bool FileExists(string fileName);
-    IEnumerable<string> ReadLinesFromFile(string fileName);
-    void WriteText(string text, string fileName);
+    private static string GetSecretsPath(string fileName) => Path.Combine("Secrets", fileName);
 }
