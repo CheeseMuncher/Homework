9d900ffec19a7cc5a0d0c2a7549f05e3cfaf30fb
CheeseMuncher <tomowen84@yahoo.co.uk>
2022-12-30 16:32:58 +0000

    Calculating Prices

---
+++ Finance/Finance.Tests/PriceCalculatorTests.cs
@@ -0,0 +1,71 @@
+using Finance.Domain.GoogleSheets;
+using FluentAssertions;
+using Xunit;
+
+namespace Finance.Tests;
+
+public class PriceCalculatorTests : TestFixture<PriceCalculator>
+{
+    [Fact]
+    public void CalculatePrice_ReturnsZero_IfUnitsZero()
+    {
+        // Arrange
+        var row = new LedgerRowBuilder().WithUnits(0).Build();
+        
+        // Act
+        var result = Sut.CalculatePrice(row);
+
+        // Assert
+        result.Should().Be(0m);
+    }
+
+    [Fact]
+    public void CalculatePrice_CalculatesBuyPriceCorrectly()
+    {
+        // Arrange
+        var row = new LedgerRowBuilder()
+            .WithConsideration(-8959.85m)
+            .WithUnits(490)
+            .Build();
+        
+        // Act
+        var result = Sut.CalculatePrice(row);
+
+        // Assert
+        result.ToString("0.000").Should().Be("1828.541");
+    }
+
+    [Fact]
+    public void CalculatePrice_CalculatesSellPriceCorrectly()
+    {
+        // Arrange
+        var row = new LedgerRowBuilder()
+            .WithConsideration(15617.97m)
+            .WithUnits(-490)
+            .Build();
+        
+        // Act
+        var result = Sut.CalculatePrice(row);
+
+        // Assert
+        result.ToString("0.000").Should().Be("3187.341");
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(-1)]
+    public void CalculatePrice_IgnoresConsiderationBelow2k(int sign)
+    {
+        // Arrange
+        var row = new LedgerRowBuilder()
+            .WithConsideration(sign * 1999.99m)
+            .WithUnits(-1 * sign * 100)
+            .Build();
+        
+        // Act
+        var result = Sut.CalculatePrice(row);
+
+        // Assert
+        result.Should().Be(0);
+    }
+}
---
+++ Finance/Finance/Domain/GoogleSheets/PriceCalculator.cs
@@ -0,0 +1,22 @@
+using Finance.Domain.GoogleSheets.Models;
+
+namespace Finance.Domain.GoogleSheets;
+
+public interface IPriceCalculator
+{
+    decimal CalculatePrice(LedgerInputRow row);
+}
+
+public class PriceCalculator : IPriceCalculator
+{
+    public decimal CalculatePrice(LedgerInputRow row)
+    {
+        if (row.Units == 0)
+            return 0;
+
+        if (Math.Abs(row.Consideration) < 2000m)
+            return 0;
+
+        return -100 * row.Consideration / row.Units;
+    }
+}
