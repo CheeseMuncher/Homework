984d49c4d5fa2ddda7c3faa3a242416c68ccb4bd
CheeseMuncher <tomowen84@yahoo.co.uk>
2023-01-06 23:01:52 +0000

    Sanitising inputs

--- RomanNumerals/RomanNumerals/Program.cs
+++ RomanNumerals/RomanNumerals/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RomanNumerals;
+
+var validator = new InputValidator();
+var input = "";
+var validationOutput = validator.IsValid(input);
+
+while (!validationOutput.IsValid)
+{
+    Console.WriteLine("Type an integer between 1 and 2000 and press enter");
+    input = Console.ReadLine();
+    validationOutput = validator.IsValid(input);
+    if (!validationOutput.IsValid)
+        Console.WriteLine(validationOutput.Message);
+    else
+        break;
+}
