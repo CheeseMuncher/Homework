192714510deeb51a40b03895196616ef09044dee
CheeseMuncher <tomowen84@yahoo.co.uk>
2023-02-23 14:15:11 +0000

    FizzBuzz With A Twist Part 3

---
+++ FizzBuzz/FizzBuzzBar/Program.cs
@@ -0,0 +1,15 @@
+﻿using TwistedFizzBuzz;
+
+var _twistedFizzBuzz = new TwistedFizzBuzz.TwistedFizzBuzz();
+var rules = new []
+{
+    new FizzBuzzRule { Multiple = 5, Word = "Fizz" },
+    new FizzBuzzRule { Multiple = 9, Word = "Buzz" },
+    new FizzBuzzRule { Multiple = 27, Word = "Bar" }
+};
+_twistedFizzBuzz.SetRules(rules);
+
+foreach(var text in _twistedFizzBuzz.GetRangeOutput(-20,127))
+{
+    Console.WriteLine(text);
+}
